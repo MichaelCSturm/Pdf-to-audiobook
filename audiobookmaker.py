e2b8ae016b610ac488e0de4113893f3eb3619165
Michael Sturm <59833741+MichaelCSturm@users.noreply.github.com>
2024-06-03 12:52:21 -0500

    Add files via upload

---
+++ audiobookmaker.py
@@ -0,0 +1,27 @@
+import pyttsx3,PyPDF2 
+pdfObj = open('chenjiahao-Real-Time-Rendering-Fourth-Edition-2018-CRC-Press.pdf','rb') 
+pdfreader = PyPDF2.PdfReader(pdfObj) 
+speaker = pyttsx3.init() 
+audiobook = ""
+numChapter = 0
+for page_num in range(len(pdfreader.pages)):  
+    numChapter+=1
+    text = pdfreader.pages[page_num].extract_text()  ## extracting text from the PDF 
+    cleaned_text = text.strip().replace('\n',' ')  ## Removes unnecessary spaces and break lines 
+    
+    print(numChapter)          
+    #speaker.say(cleaned_text)        ## Let The Speaker Speak The Text 
+    audiobook = audiobook + cleaned_text
+    
+    
+    # if( cleaned_text.find("Chapter 1 Introduction") or  cleaned_text.find("Chapter 2 The Graphics Rendering Pipeline") or cleaned_text.find("Chapter 3 The Graphics Processing Unit") or
+    #    cleaned_text.find("") or cleaned_text.find("") or cleaned_text.find("") or
+    #    cleaned_text.find("") or cleaned_text.find("") or cleaned_text.find("") or
+    #    cleaned_text.find("") or cleaned_text.find("") or cleaned_text.find("") or
+    #    cleaned_text.find("") or cleaned_text.find("") or cleaned_text.find("") ):
+    #     fileName = str(numChapter)+ ".mp3" 
+
+    speaker.save_to_file(cleaned_text,"Page" +str(numChapter) +".mp3")  ## Saving Text In a audio file 'story.mp3' 
+    speaker.runAndWait()
+
+     
